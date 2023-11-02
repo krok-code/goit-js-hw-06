275d75c2908dc2c989f5c80a6deb3f94c9fc791a
krok-code <maks.smolinskyi@gmail.com>
2023-11-02 23:16:16 +0200

    two tasks completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,28 @@
+const controlsDiv = document.getElementById("controls");
+const boxesDiv = document.getElementById("boxes");
+
+const input = controlsDiv.querySelector("input");
+const createButton = controlsDiv.querySelector("[data-create]");
+const destroyButton = controlsDiv.querySelector("[data-destroy]");
+
+createButton.addEventListener("click", () => createBoxes(input.value));
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = Number(amount);
+  let divs = '';
+  for (let i = 0; i < amount; i++) {
+    const size = 30 + i * 10;
+    const color = getRandomHexColor();
+    divs += `<div style="width: ${size}px; height: ${size}px; background-color: ${color};"></div>`;
+  }
+  boxesDiv.insertAdjacentHTML("beforeend", divs);
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
 }
