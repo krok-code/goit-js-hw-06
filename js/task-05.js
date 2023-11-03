af6df6e296c53c599cdd990011144592895318d7
krok-code <maks.smolinskyi@gmail.com>
2023-11-03 10:34:46 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById("name-output");
 
 nameInput.addEventListener("input", () => {
-	const inputValue = nameInput.ariaValue;
+	const inputValue = nameInput.value;
 
 	if (inputValue.trim() === "") {
 		nameOutput.textContent = "Anonymous";
