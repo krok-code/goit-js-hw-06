ddcd949840440828c0be03e9ceba86bd0703cb0c
krok-code <maks.smolinskyi@gmail.com>
2023-11-03 10:42:39 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const fontSizeControl = document.getElementById("font-size-control");
 const textSpan = document.getElementById("text");
 
-fontSizeControl.addEventListener("inut", () => {
+fontSizeControl.addEventListener("input", () => {
 	const fontSize = fontSizeControl.value;
 
 	textSpan.style.fontSize = `${fontSize}px`;
