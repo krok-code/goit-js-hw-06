a8770044d83acb4af42f99b44037ef0ac82947f5
krok-code <maks.smolinskyi@gmail.com>
2023-11-03 10:31:38 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,22 +1,22 @@
 const counter = document.getElementById("counter");
 const valueSpan = document.getElementById("value");
-const incrementButton = document.querySelector('[data-action="increment"]');
-const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = counter.querySelector('[data-action="increment"]');
+const decrementButton = counter.querySelector('[data-action="decrement"]');
 
 let counterValue = 0;
 
 function updateCounter() {
-	valueSpan.textContent = counterValue;
+  valueSpan.textContent = counterValue;
 }
 
 incrementButton.addEventListener("click", () => {
-	counterValue += 1;
-	updateCounter();
+  counterValue += 1;
+  updateCounter();
 });
 
 decrementButton.addEventListener("click", () => {
-	counterValue -= 1;
-	updateCounter();
+  counterValue -= 1;
+  updateCounter();
 });
 
 updateCounter();
